d4e641bae0c9ccc06e852f3554430be3cfe76973
Denny Tek <tek.denny@gmail.com>
2016-06-27 12:15:44 -0400

    Populate edit menu

--- source/js/fb-editable.js
+++ source/js/fb-editable.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 
+  var dbRef = firebase.database().ref('/');
+  var conferencesRef = firebase.database().ref('conferences/');
+
+
   // AUTHENTICATION
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -55,9 +59,10 @@ $(document).ready(function() {
     $("#auth").show();
     $('#current-user').text(user.email);
     $("#logout").show();
+    populateEditMenu("#data-menu");
   }
 
-  // MESSAGE HANDLING
+  // Message Handling
   function handleAdminErrors(err, view) {
     $(view).text(err);
   }
@@ -68,5 +73,51 @@ $(document).ready(function() {
        $('#data-panel').fadeOut();
     },1000);
   }
+
+  // Error Handling
+  var onComplete = function(error) {
+    if (error) {
+      console.log('Synchronization failed');
+      console.log(error);
+      handleAdminErrors(error.valueOf(), "#login-message");
+      alert(error.valueOf());
+    } else {
+      console.log('Synchronization succeeded');
+      messageHandler("Synchronization succeeded");
+      $("#data-menu").empty();      
+      populateEditMenu("#data-menu");
+    }
+  };
+
+  // Populate Edit Menu
+  function populateEditMenu(view) {
+    dbRef.once("value",function(snapshot){
+      snapshot.forEach(function(snap) {
+        $(view).append("<div class='"+ snap.key +"'><h3>" + snap.key + "</h3></div>");
+      });
+      snapshot.forEach(function(snap) {
+        snap.forEach(function(s) {
+          var selector = snap.key;
+          if (selector == s.ref.parent.key) {
+            $("div[class*='"+selector+"']").append("<a id='"+s.key+"' class='list-menu-item b-button '"+ s.ref.parent.key +">"+ s.val().title +"</a>");
+          }
+        });
+      });
+    });
+  }
+
+
+
+  // CREATE
+  function addNewConference(attr) {
+    conferencesRef.push({
+      data: data
+    });
+  }
+
+
+
+
+
 
 });
